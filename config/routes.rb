02e24579dc63aac912631495af57240cb42151ef
BlueHead Publishing <da@blueheadpublishing.com>
2011-07-16 16:37:49 -0700

    initial import to github

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 WorldUnitingOrg::Application.routes.draw do
-  resources :about  
+  
+  match 'about/mission_vision' => 'about/mission_vision#index'
+  match 'about/board_staff' => 'about/board_staff#index'
+  match 'about/opportunities' => 'about/opportunities#index'
+  match 'about/financials_downloads' => 'about/financials_downloads#index'
+  
+  resources :about
+  resources :programs
+  resources :contact
+  resources :get_involved
+  
   
   get "home/index"
   root :to => 'home#index'
